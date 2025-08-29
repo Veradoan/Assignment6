87e2d24158a6456a16f559c4056d555d0abe931e
Veradoan <tuyen03052006@gmail.com>
2025-08-29 22:49:06 +0700

    Update ConvertBetweenArrayAndArrayList.java

--- src/exercise6/ConvertBetweenArrayAndArrayList.java
+++ src/exercise6/ConvertBetweenArrayAndArrayList.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exercise6;
 
-/**
- *
- * @author Admin
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ConvertBetweenArrayAndArrayList {
-    
+
+   
+   public static ArrayList<String> arrayToArrayList(String[] array) {
+        ArrayList<String> list = new ArrayList<>();
+        for (String element : array) {
+            list.add(element);
+        }
+        return list;
+    }
+
+    public static String[] arrayListToArray(ArrayList<String> list) {
+        return list.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+     
+        String[] array = {"Ann", "Bob"};
+        ArrayList<String> list = arrayToArrayList(array);
+        System.out.println("arrayToArrayList with " + Arrays.toString(array) +
+                           " -> returns an ArrayList containing " + list);
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Charles");
+        list2.add("David");
+        String[] array2 = arrayListToArray(list2);
+
+        System.out.println("arrayListToArray with " + list2 +
+                           " -> returns an array " + Arrays.toString(array2));
+    }
 }
